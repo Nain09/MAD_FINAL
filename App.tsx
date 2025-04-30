9a1657819bd84621badeb5a48b2c6de2030e02eb
Nain09 <s22210396@student.unklab.ac.id>
2025-04-30 22:23:41 +0800

    membuat navigator untuk semua halaman

--- App.tsx
+++ App.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App: React.FC = () => {
+import SignIn from './page/SignIn';
+import SignUp from './page/SignUp';
+import Home from './page/Home';
+import CarDetail from './page/CarDetail';
+import Purchase from './page/Purchase';
+import Profile from './page/Profile';
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Beranda'}}
+        />
+        <Stack.Screen
+          name="CarDetail"
+          component={CarDetail}
+          options={{title: 'Detail Mobil'}}
+        />
+        <Stack.Screen
+          name="Purchase"
+          component={Purchase}
+          options={{title: 'Pembelian'}}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{title: 'Profil Saya'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
