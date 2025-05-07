f23d69bdea90166682e21b9e2ad7bed05f1779a2
evanjelio <S22210700@student.unklab.ac.id>
2025-05-07 13:33:05 +0800

    menambahkan search bar

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -6,7 +6,7 @@ import {
   FlatList,
   SafeAreaView,
   TouchableOpacity,
-  Alert,
+  TextInput,
 } from 'react-native';
 import CarCard from '../../src/components/molecules/CarCard';
 import PrimaryButton from '../../src/components/atoms/PrimaryButton';
@@ -33,6 +33,12 @@ const dummyCars = [
 ];
 
 const Home = ({navigation}) => {
+  const [searchText, setSearchText] = useState('');
+
+  const filteredCars = dummyCars.filter(car =>
+    car.name.toLowerCase().includes(searchText.toLowerCase()),
+  );
+
   const renderItem = ({item}) => (
     <CarCard
       car={item}
@@ -43,8 +49,17 @@ const Home = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.header}>Daftar Mobil Dijual</Text>
+
+      {/* Input Pencarian */}
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Cari Mobil..."
+        value={searchText}
+        onChangeText={setSearchText}
+      />
+
       <FlatList
-        data={dummyCars}
+        data={filteredCars}
         renderItem={renderItem}
         keyExtractor={item => item.id}
         contentContainerStyle={styles.list}
@@ -76,6 +91,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginHorizontal: 16,
     marginBottom: 12,
+  },
+  searchBar: {
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginHorizontal: 16,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
   },
   list: {
     paddingHorizontal: 16,
